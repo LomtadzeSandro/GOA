b8b2f1960b39eeec328f72790e41e3f8e45f89c3
Sandro Lomtadze <130504752+LomtadzeSandro@users.noreply.github.com>
2024-02-20 22:53:59 +0400

    Add files via upload

---
+++ Day1/Lesson1/Homework.py
@@ -0,0 +1,10 @@
+# print()-ის საშუალებით გამოვიტანოთ გრძელი წინადადება
+name = "Sandro"
+surname = "Lomtadze"
+age = 20
+height = 1.79
+loves_programming = True
+loves_cooking = False
+
+print("My name is " + name + " My surname is " + surname + " I am " + str(age) + " years old " + "My height is " + str(height) + " height " + " loves_programming " + "loves_cooking")
+
