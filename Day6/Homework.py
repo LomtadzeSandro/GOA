6593c423a88bea745153fecc803721dadc14bd00
Sandro Lomtadze <130504752+LomtadzeSandro@users.noreply.github.com>
2024-02-25 22:24:15 +0400

    Add files via upload

---
+++ Day6/Homework.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+surname = input("Please enter your surname: ")
+age = input("Please enter your age: ")
+introduction = "Hello i am " + name + " " + surname + " " + " i am " + str(age) + " years old"
+
+print(introduction)
