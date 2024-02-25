768b1d60dccd1b6adde078e5318184472c72fca7
Sandro Lomtadze <130504752+LomtadzeSandro@users.noreply.github.com>
2024-02-25 22:25:45 +0400

    Add files via upload

---
+++ Day9/classwork.py
@@ -0,0 +1,9 @@
+# password = 12345
+# guess = int (input("sheiyvanet paroli"))
+# while guess != password :
+#     access = "granted"
+#     print(access)
+# if(guess == password):
+#     print("succesfull")
+birth_year = input()
+print(type(birth_year))
