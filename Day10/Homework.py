5cd71e2a6ae7480acd24aba945112875c806c063
Sandro Lomtadze <130504752+LomtadzeSandro@users.noreply.github.com>
2024-02-29 18:14:41 +0400

    Add files via upload

---
+++ Day10/Homework.py
@@ -0,0 +1,12 @@
+password = 12345678
+while True:
+    password_1 = int(input("shemoiyvanet paroli"))
+    if password_1 == password:
+        print("true")
+        break
+    else:
+        print("try Again")
+        
+          
+
+        
