9a52a64998030257ac26afd3a88990afeb18e6de
Sandro Lomtadze <130504752+LomtadzeSandro@users.noreply.github.com>
2024-03-30 11:48:03 +0400

    Add files via upload

---
+++ Day17/lesson.py
@@ -0,0 +1,50 @@
+# num = int(input("Enter a number: "))
+# total_sum = 0
+
+# for i in range(1, num + 1):
+#     total_sum += i
+
+# print("The sum from 1 to", num, "is:", total_sum)
+
+
+# def square_even_cube_odd():
+#     num1 = int(input("Enter the first number: "))
+#     num2 = int(input("Enter the second number: "))
+    
+#     if num1 > num2:
+#         num1, num2 = num2, num1
+    
+#     result = 0
+    
+#     for num in range(num1 + 1, num2):
+#         if num % 2 == 0:
+#             result += num ** 2
+#         else:
+#             result += num ** 3
+    
+#     print("Result:", result)
+
+# square_even_cube_odd()
+
+# def increase_character_name():
+#    a = input("Enter Name: ")
+#    if(a.index[0])
+
+
+# def triangle():
+#   a = int(input("შეიყვანეთ პირველი გვერდი"))
+#   b = int(input("შეიყვანეთ მეორე გვერდი"))
+#   c = int(input("შეიყვანეთ მესამე გვერდი"))
+#   if(a + b) > c:
+#     print("ასეთი სამკუთხედი არსებობს")
+#   elif (a + b) == c:
+#     print("ასეთი სამკუთხედი არ არსებობს")
+#   else:
+#     print("ასეთი სამკუთხედი არ არსებობს")
+
+# triangle()
+
+  
+  
+
+
---
+++ Day18/hakaton.py
@@ -0,0 +1,53 @@
+# num1 = int(input("Enter number: "))
+# num2 = int(input("Enter second number: "))
+# print(num1 + num2)
+
+# num1 = int(input("Enter number: "))
+# num2 = int(input("Enter second number: "))
+# print(num1**num2)
+ 
+
+# array = [int(i) for i in input("Pleace enter first number: ").split()]
+# array2 = [int(i) for i in input("Pleace enter second number: ").split()]
+# x = [max(array, array2)]
+# y = [min(array, array2)]
+# print(x - y)
+
+
+# a = int(input("Enter number: "))
+# b = int(input("Enter number: "))
+# c = int(input("Enter number: "))
+# if c % b == 0 and c % a == 0:
+#   print("correct")
+# else:
+#   print("incorrect")
+ 
+# array = []
+
+
+# a = [input("Enter word: ")]
+# b = input("Enter character: ")
+# for char in b:
+#   counted = a.count()
+  
+
+# num_list = [int(x) for x in input("Enter the list").split()]
+# sorted_list = sorted [num_list, reverse = True]
+
+# def max_numbers(lst):
+
+x = 1
+a = int(input("enter number: "))
+while x % 1 == 0 and a % x == 0:
+  x = x + 1
+  if x % 1 == 0 and a % x == 0:
+    print("not prime")
+    break
+  else :
+    print("prime")
+
+
+
+
+
+  
