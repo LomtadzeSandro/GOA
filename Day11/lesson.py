85bb2b4c906f69c2216725f59424e219dcbcfb87
Sandro Lomtadze <130504752+LomtadzeSandro@users.noreply.github.com>
2024-03-30 11:46:23 +0400

    Add files via upload

---
+++ Day11/lesson.py
@@ -0,0 +1,20 @@
+# დაწერეთ პროგფრამა რომელიც მიიღებს მომხმარებლისგან რიცხვს ეს რიცხვი არის დადებიტი უარყოფითი ან ნული თუ იქნება დადებოთი დაპრინტოს პოზიტივ თუ ნეგატივე დაპრინტოს ნეგატივ და თუ 0 დაპრინტოს 0
+# a = int(input("Enter number: "))
+# if a > 0:
+#     print("this number is postive")
+# elif a < 0:
+#     print("this number is negative")
+# else:
+#     print("this number equals 0")
+
+
+
+# for i in range(1,20):
+#   if i % 2 == 0:
+#     print(i)
+
+
+num = int(input("Enter number: "))
+
+    
+    
